b10bb6ce95efa8b3cab79184d205dd00c2fe02bb
Daniele Ongari <daniele.ongari@gmail.com>
2019-03-11 20:24:34 +0100

    Package making - part 2: setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+
+if __name__ == '__main__':
+    setup(
+        packages=find_packages(),
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        name="calc_pe",
+        author="Daniele Ongari",
+        author_email="daniele.ongari@epfl.ch",
+        description="Calculator for the CO2 parasitic energy, given CO2 and N2 isotherms",
+        url="https://github.com/danieleongari/calc_pe",
+        license="Creative Commons",
+        classifiers=["Programming Language :: Python"],
+        version="0.1.0",
+        install_requires=['numpy',
+                          'pandas>=0.24.0',
+                          'pyiast'
+                         ],
+        scripts=["calc_pe/run_pe"],
+        )
